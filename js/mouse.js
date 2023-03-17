62629ecf6b566eeeadde704296dc0fc1c35909fb
ivan.vorontsov <linelander@icloud.com>
2023-03-17 19:47:32 +0200

    MOUSE UPD

--- js/mouse.js
+++ js/mouse.js
@@ -1,5 +1,6 @@
 let mousePosition = { x: 0, y: 0 },
     mousePressed = false,
+    time0,
     isTouchDevice = 'ontouchstart' in window || navigator.msMaxTouchPoints > 0;
 
 function handleMouseMove(evt) {
@@ -25,11 +26,16 @@ function handleTouchStart(evt) {
     mousePressed = true;
     mousePosition.x = (evt.touches[0].clientX - offsetLeft) / APP.scaleX;
     mousePosition.y = (evt.touches[0].clientY - offsetTop) / APP.scaleY;
+    time0 = new Date();
 }
 
 function handleTouchEnd(evt) {
     mousePressed = false;
-    //toggleFullscreeen();
+    let t = new Date();
+    let diff = t.getTime() - time0.getTime();
+    if (diff < 200) {
+        pointer.set();
+    }
 }
 
 function handleClick(evt) {
